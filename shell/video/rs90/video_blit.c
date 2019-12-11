c69940e26056a6bb271cb5f2e202ae34f3d0dac9
gameblabla <gameblabla@openmailbox.org>
2019-12-11 19:58:18 +0100

    Fix up RS-90 code.

--- shell/video/rs90/video_blit.c
+++ shell/video/rs90/video_blit.c
@@ -45,7 +45,7 @@ void Init_Video()
 	
 	backbuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, HOST_WIDTH_RESOLUTION, HOST_HEIGHT_RESOLUTION, 16, 0,0,0,0);
 	
-	ngp_vs = SDL_CreateRGBSurface(SDL_SWSURFACE, INTERNAL_NGP_WIDTH, INTERNAL_NGP_HEIGHT, 16, 0,0,0,0);
+	ngp_vs = SDL_CreateRGBSurface(SDL_SWSURFACE, INTERNAL_NGP_WIDTH, INTERNAL_NGP_HEIGHT+1, 16, 0,0,0,0);
 	
 	Set_Video_InGame();
 }
@@ -99,6 +99,14 @@ void Update_Video_Ingame()
 	{
 		// Fullscreen
 		case 0:
+			bitmap_scale(0, 0, internal_width, internal_height, HOST_WIDTH_RESOLUTION, HOST_HEIGHT_RESOLUTION, internal_width, 0, (uint16_t* restrict)source_graph, (uint16_t* restrict)sdl_screen->pixels);
+		break;
+		// Keep Aspect
+		case 1:
+			bitmap_scale(0,0,internal_width,internal_height,keep_aspect_width,keep_aspect_height,internal_width, HOST_WIDTH_RESOLUTION - keep_aspect_width,(uint16_t* restrict)source_graph,(uint16_t* restrict)sdl_screen->pixels+(HOST_WIDTH_RESOLUTION-keep_aspect_width)/2+(HOST_HEIGHT_RESOLUTION-keep_aspect_height)/2*HOST_WIDTH_RESOLUTION);
+		break;
+		// Native
+		case 2:
 			pitch = HOST_WIDTH_RESOLUTION;
 			src = (uint16_t* restrict)ngp_vs->pixels;
 			dst = (uint16_t* restrict)sdl_screen->pixels
@@ -110,13 +118,6 @@ void Update_Video_Ingame()
 				src += internal_width;
 				dst += pitch;
 			}
-		break;
-		// Fullscreen
-		case 1:
-			bitmap_scale(0, 0, internal_width, internal_height, HOST_WIDTH_RESOLUTION, HOST_HEIGHT_RESOLUTION, internal_width, 0, (uint16_t* restrict)source_graph, (uint16_t* restrict)sdl_screen->pixels);
-		break;
-		case 2:
-			bitmap_scale(0,0,internal_width,internal_height,keep_aspect_width,keep_aspect_height,internal_width, HOST_WIDTH_RESOLUTION - keep_aspect_width,(uint16_t* restrict)source_graph,(uint16_t* restrict)sdl_screen->pixels+(HOST_WIDTH_RESOLUTION-keep_aspect_width)/2+(HOST_HEIGHT_RESOLUTION-keep_aspect_height)/2*HOST_WIDTH_RESOLUTION);
 		break;
 		// Hqx
 		case 3:
