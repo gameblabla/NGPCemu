6e75bdeb6e4b724a22e4449efcf41b4c7a7b675f
gameblabla <gameblabla@openmailbox.org>
2019-12-11 19:59:15 +0100

    Fix up RS-97 code (with memory leak fix).

--- shell/video/rs97/video_blit.c
+++ shell/video/rs97/video_blit.c
@@ -49,7 +49,7 @@ void Init_Video()
 	
 	backbuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, HOST_WIDTH_RESOLUTION, HOST_HEIGHT_RESOLUTION, 16, 0,0,0,0);
 	
-	ngp_vs = SDL_CreateRGBSurface(SDL_SWSURFACE, INTERNAL_NGP_WIDTH, INTERNAL_NGP_HEIGHT, 16, 0,0,0,0);
+	ngp_vs = SDL_CreateRGBSurface(SDL_SWSURFACE, INTERNAL_NGP_WIDTH, INTERNAL_NGP_HEIGHT+1, 16, 0,0,0,0);
 	
 	Set_Video_InGame();
 }
@@ -104,6 +104,14 @@ void Update_Video_Ingame()
 		{
 			// Fullscreen
 			case 0:
+				upscale_160x152_to_320x240((uint32_t* restrict)sdl_screen->pixels, (uint32_t* restrict)ngp_vs->pixels);
+			break;
+			// Keep Aspect
+			case 1:
+				bitmap_scale(0,0,internal_width,internal_height,keep_aspect_width,keep_aspect_height,internal_width, HOST_WIDTH_RESOLUTION - keep_aspect_width,(uint16_t* restrict)source_graph,(uint16_t* restrict)sdl_screen->pixels+(HOST_WIDTH_RESOLUTION-keep_aspect_width)/2+(HOST_HEIGHT_RESOLUTION-keep_aspect_height)/2*HOST_WIDTH_RESOLUTION);
+			break;
+			// Native
+			case 2:
 				pitch = HOST_WIDTH_RESOLUTION;
 				src = (uint16_t* restrict)ngp_vs->pixels;
 				dst = (uint16_t* restrict)sdl_screen->pixels
@@ -115,13 +123,6 @@ void Update_Video_Ingame()
 					src += internal_width;
 					dst += pitch;
 				}
-			break;
-			// Fullscreen
-			case 1:
-				upscale_160x152_to_320x240((uint32_t* restrict)sdl_screen->pixels, (uint32_t* restrict)ngp_vs->pixels);
-			break;
-			case 2:
-				bitmap_scale(0,0,internal_width,internal_height,keep_aspect_width,keep_aspect_height,internal_width, HOST_WIDTH_RESOLUTION - keep_aspect_width,(uint16_t* restrict)source_graph,(uint16_t* restrict)sdl_screen->pixels+(HOST_WIDTH_RESOLUTION-keep_aspect_width)/2+(HOST_HEIGHT_RESOLUTION-keep_aspect_height)/2*HOST_WIDTH_RESOLUTION);
 			break;
 			// Hqx
 			case 3:
